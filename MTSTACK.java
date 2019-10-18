8ef6a2951cd8cb0f37cdd5b957df415617df4ef9
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-10-18 14:26:22 -0400

    Update MTSTACK.java

--- MTSTACK.java
+++ MTSTACK.java
@@ -4,10 +4,13 @@ public class MTSTACK<X> implements ISTACK<X>{
 	public MTSTACK() {};  
 	public X pop() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
-	public MTSTACK<X> push (X v){return(new MTSTACK<X>(v,this));}
-	public MTSTACK<X> reverse() {return this;}
+	public MTSTACK<X> push (X v){return(new NMTSTACK<X>(v,this));}
 	public X top() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
+	public boolean emptyStack() {return true;}
+}
+	
+	
 	
 	
 
