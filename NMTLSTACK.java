86a6ace9ba2ba70ecb6e0b96b65a01151df11202
eNav <56284337+eNav@users.noreply.github.com>
2019-10-21 19:30:31 -0400

    Update NMTLSTACK.java

--- NMTLSTACK.java
+++ NMTLSTACK.java
@@ -1,24 +1,28 @@
 package Stack;
 
 public class NMTSTACK<X> implements ISTACK<X> {
-	private X car;
-	private ISTACK<X> cdr;
+	private X car; //first of the list
+	private ISTACK<X> cdr; // rest of the list
 	
 	public NMTSTACK(X f, ISTACK<X> r) {
-		car = f;
-		cdr = r;	}
+		car = f;//first of the list
+		cdr = r;//restt of the list	}
 	
+	//Purpose: to push an element on a stack, (putting an new element on top). var X is our list and local variable v get puts on top of the stack.
 	public ISTACK<X> push(X v){
-		return new NMTSTACK<X>(v,this);	}
+		return new NMTSTACK<X>(v,this);	}//Returns a new non empty stack with new element as the first element 
 	
-	public ISTACK<X> pop() throws Exception {
-		return this.cdr;
-	}	
-	public X top() throws Exception{return car;}
+	//Purpose: To pop a stack which returns the rest of the list.
+	public ISTACK<X> pop() throws Exception {//if the rest is empty, then return the exception
+		return this.cdr;//cdr is a private variable that returns rest of list. 
+	}
+		
+	//Purpose: Returns the first element or "top" of the stack. Does not get changed in any way.	
+	public X top() throws Exception{return car;}//Exception used to try to catch a error. 
 	
-	public ISTACK<X> stackrest() throws Exception{return cdr;}
 	
-	public boolean emptyStack() {return false;}
+	//Purpose: To check if the stack is empty or not
+	public boolean emptyStack() {return false;}// returns false because the non empty stack will lever be empty.
 	
 		private ISTACK<X> revhelper(ISTACK<X> l, ISTACK<X>res)
 	//ACCUM INV: reverse(this) == append(reverse(l), res)
