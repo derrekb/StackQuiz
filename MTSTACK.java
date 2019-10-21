b412647441adde51e081803fa325b500cf9a3990
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-10-21 18:33:06 -0400

    new changes

--- MTSTACK.java
+++ MTSTACK.java
@@ -6,26 +6,29 @@ public class MTSTACK<X> implements ISTACK<X> {
 		throw new Exception("Method pop applied to an empty stack");} //used to throw an error if pop is used on an empty stack
 
 	public MTSTACK() {};
+	
+	//Purpose: To throw an exception because you cant retrieve the the rest of a stack
 	public X pop() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
 	public MTSTACK<X> push (X v){return(new NMTSTACK<X>(v,this));}
 	public X top() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
+	
 	public boolean emptyStack() {return true;}
 }
 
-
-
+//Purpose: //used to throw an error if top is used on an empty stack otherwise, it gets the top of the stack
 	public X top() throws Exception{
-		throw new Exception("Method first applied to an empty stack");} //used to throw an error if top is used on an empty stack
-	//otherwise, it gets the top of the stack
-
+		throw new Exception("Method first applied to an empty stack");} 
+	
+	//Purpose: method used to determine if a stack is empty
 	public boolean emptyStack() {return true;}
-	//method used to determine if a stack is empty
+	
 
-	public ISTACK<X> reverse() {return this;} //reverses the stack
+	//Purpose: reverses the stack
+	public ISTACK<X> reverse() {return this;} 
 
-	//used to get all the elements from the stack but if the stack is empty, it returns an error
+	//Purpose: used to get all the elements from the stack but if the stack is empty, it returns an error
 	public ISTACK<X> stackrest() throws Exception {
 		throw new Exception("Method first applied to an empty stack");
 
