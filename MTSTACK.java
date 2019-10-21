b36c766d0c9fab12d6df01aad584b4cfffa7ba94
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-10-21 18:46:49 -0400

    Update MTSTACK.java

--- MTSTACK.java
+++ MTSTACK.java
@@ -1,31 +1,30 @@
 package Stack;
-
 public class MTSTACK<X> implements ISTACK<X> {
-	public MTSTACK() {}; //initializes an emtpy stack
+	//Purpose: initializes an emtpy stack
+	public MTSTACK() {}; 
+	//Purpose: To pop a stack (returns rest of a stack)
 	public ISTACK<X> pop() throws Exception{
 		throw new Exception("Method pop applied to an empty stack");} //used to throw an error if pop is used on an empty stack
 
-	public MTSTACK() {};
 	
-	//Purpose: To throw an exception because you cant retrieve the the rest of a stack
-	public X pop() throws Exception{
-		throw new Exception("Method first applied to an empty stack");}
+	//Purpose: To push an element on a stack. Pushing places an element on top of a stack-> outputs a new stack
+	//X: is the empty stack
+	//v: is a local variable which represents the element being pushed to the top of the empty stack
 	public MTSTACK<X> push (X v){return(new NMTSTACK<X>(v,this));}
+	
+	
+	//Purpose: A check to determine if an empty stack is empty -> returns true
+	public boolean emptyStack() {return true;}
+
+//Purpose: used to throw an error if top is used on an empty stack otherwise, it gets the top of the stack
 	public X top() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
-	
-	public boolean emptyStack() {return true;}
-}
-
-//Purpose: //used to throw an error if top is used on an empty stack otherwise, it gets the top of the stack
-	public X top() throws Exception{
-		throw new Exception("Method first applied to an empty stack");} 
 	
 	//Purpose: method used to determine if a stack is empty
 	public boolean emptyStack() {return true;}
 	
 
-	//Purpose: reverses the stack
+	//Purpose: reverses the stack-> Returns the stack
 	public ISTACK<X> reverse() {return this;} 
 
 	//Purpose: used to get all the elements from the stack but if the stack is empty, it returns an error
@@ -33,7 +32,8 @@ public class MTSTACK<X> implements ISTACK<X> {
 		throw new Exception("Method first applied to an empty stack");
 
 	}
-	public String ToString() {return null;} //?
+	//Purpose: Outputs the contents of a stack into a string
+	public String ToString() {return null;} 
 
 
 }
