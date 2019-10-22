e9d20578f63cb0f50a22305d728c287e444b47aa
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-10-22 18:20:58 -0400

    Update StackIFun.java

--- StackIFun.java
+++ StackIFun.java
@@ -1,5 +1,10 @@
 package Stack;
-//hey
+/**
+ * Purpose: Supplementary Interface for Stack : with list
+ *
+ * @param <X>
+ * @param <Y>
+ */
 public interface StackIFun<X, Y> {
 	public Y f(X val);
 }
